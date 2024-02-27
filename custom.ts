35af5a3f5d636f0a60012739423ac7fae66dc579
evanstonestreet <123288382+evanstonestreet@users.noreply.github.com>
2024-02-27 18:35:41 -0500

    Update pxt.json, main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -17,31 +17,96 @@ enum MyEnum {
 //% weight=100 color=#0fbc11 icon=""
 namespace custom {
     /**
-     * TODO: describe your function here
+     * TODO: Initializes program with variables
      * @param n describe parameter here, eg: 5
      * @param s describe parameter here, eg: "Hello"
      * @param e describe parameter here
      */
     //% block="Initialization"
     export function Initialization(): void {
-        let Training = 0
-        let On = 0
-        let dataSessions = []
-        let Training_Name = 0
-        serial.writeString("Which Activity do you want to train?")
-        serial.writeLine("")
-        serial.writeString("Button A: Motor turn 90 degrees CW")
-        serial.writeLine("")
-        serial.writeString("Button B: Motor turn 90 degrees CC")
+        serial.writeString("Would you like to Train or Test? (Button A for Train, Button B for Test)")
         serial.writeLine("")
     }
 
     /**
-     * TODO: describe your function here
+     * TODO: Allows user to train the model
      * @param value describe value here, eg: 5
      */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    //% block="Train"
+    export function train(): void {
+        if (On == 1) {
+            serial.writeString("We are training now")
+            serial.writeLine("")
+            input.setAccelerometerRange(AcceleratorRange.OneG)
+            serial.writeNumber(input.acceleration(Dimension.X))
+            currentSession.push(input.acceleration(Dimension.X))
+            serial.writeString(",")
+            serial.writeNumber(input.acceleration(Dimension.Y))
+            currentSession.push(input.acceleration(Dimension.Y))
+            serial.writeString(",")
+            serial.writeNumber(input.acceleration(Dimension.Z))
+            currentSession.push(input.acceleration(Dimension.Z))
+            serial.writeLine("")
+        }
+    }
+
+
+    /**
+     * TODO: Allows user to train the model
+     * @param value describe value here, eg: 5
+     */
+    //% block="Turn the motor by %degrees degrees"
+    export function activity(degrees: number): void {
+        numOfActivities = numOfActivities + 1
+    }
+
+
+    /**
+     * TODO: Allows user to train the model
+     * @param value describe value here, eg: 5
+     */
+    //% block="trainButtonA"
+    export function trainButtonA(): void {
+        if (activityNum <= numOfActivities && On == 0) {
+            serial.writeString("You are training activity ")
+            serial.writeNumber(activityNum)
+            serial.writeLine("")
+            currentSession.push(activityNum)
+            On = 1
+            serial.writeString("Start Data Collection")
+            serial.writeLine("")
+            activityNum = activityNum + 1
+            
+        }
+    }
+
+
+    /**
+     * TODO: Allows user to train the model
+     * @param value describe value here, eg: 5
+     */
+    //% block="trainButtonB"
+    export function trainButtonB(): void {
+        if (On == 1){
+            On = 0
+            serial.writeString("End Data Collection")
+            serial.writeLine("")
+            dataSessions.push(currentSession)
+            currentSession = []
+            
+        }
+
+    }
+
+
+    /**
+     * TODO: Allows user to train the model
+     * @param value describe value here, eg: 5
+     */
+    //% block="finish"
+    export function finish(): void {
+        serial.writeString("Number of activities: ")
+        serial.writeNumber(numOfActivities)
+        serial.writeLine("")
     }
 }
--- main.ts
+++ main.ts
@@ -1,56 +1,41 @@
 input.onButtonPressed(Button.A, function () {
-    if (Training == 0) {
-        serial.writeString("You picked 90 degrees CW. Press A to start training and B to stop")
-        serial.writeLine("")
-        Training = 1
-        Training_Name = 1
-        currentSession = []
-        currentSession.push(Training_Name)
-    } else {
-        On = 1
-        serial.writeString("Start Data Collection")
-        serial.writeLine("")
-    }
+    custom.trainButtonA()
 })
+function Initialization () {
+    Training = 0
+    On = 0
+    dataSessions = []
+    Training_Name = 0
+    serial.writeString("Which Activity do you want to train?")
+    serial.writeLine("")
+    serial.writeString("Button A: Motor turn 90 degrees CW")
+    serial.writeLine("")
+    serial.writeString("Button B: Motor turn 90 degrees CC")
+    serial.writeLine("")
+}
 input.onButtonPressed(Button.B, function () {
-    if (Training == 0) {
-        serial.writeString("You picked 90 degrees CC. Press A to start training and B to stop")
-        serial.writeLine("")
-        Training = 1
-        Training_Name = 2
-        currentSession = []
-        currentSession.push(Training_Name)
-    } else {
-        let dataSessions: number[][] = []
-        On = 0
-        Training = 0
-        serial.writeString("End Data Collection")
-        serial.writeLine("")
-        dataSessions.push(currentSession)
-        serial.writeString("Which Activity do you want to train?")
-        serial.writeLine("")
-        serial.writeString("Button A: Motor turn 90 degrees CW")
-        serial.writeLine("")
-        serial.writeString("Button B: Motor turn 90 degrees CC")
-        serial.writeLine("")
-    }
+    custom.trainButtonB()
 })
-let On = 0
-let currentSession: number[] = []
 let Training_Name = 0
+let On = 0
 let Training = 0
-custom.Initialization()
+let activityNum = 0
+let numOfActivities = 0
+let dataSessions: number[][]
+let currentSession: number[]
+Initialization()
 basic.forever(function () {
     if (On == 1) {
+        let currentSession2: number[] = []
         input.setAccelerometerRange(AcceleratorRange.OneG)
         serial.writeNumber(input.acceleration(Dimension.X))
-        currentSession.push(input.acceleration(Dimension.X))
+        currentSession2.push(input.acceleration(Dimension.X))
         serial.writeString(",")
         serial.writeNumber(input.acceleration(Dimension.Y))
-        currentSession.push(input.acceleration(Dimension.Y))
+        currentSession2.push(input.acceleration(Dimension.Y))
         serial.writeString(",")
         serial.writeNumber(input.acceleration(Dimension.Z))
-        currentSession.push(input.acceleration(Dimension.Z))
+        currentSession2.push(input.acceleration(Dimension.Z))
         serial.writeLine("")
     }
 })
